32ee0561031cd97290892ff32870d61788db5c2b
tzenishbek@gmail.com <tzenishbek@gmail.com>
2025-03-03 13:53:27 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Портфолио загружено!");
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener("click", function(event) {
+            event.preventDefault();
+            const section = document.querySelector(this.getAttribute("href"));
+            section.scrollIntoView({ behavior: "smooth" });
+        });
+    });
 });
